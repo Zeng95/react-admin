1d6a11ee9d5ec7e69a46cc61e0adc79f01df87d3
Leo Zeng <qingchun1995@gmail.com>
2022-03-15 11:43:50 +0800

    ci: :wrench: update the ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,38 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }
-    }
+    jest: true,
   },
   extends: [
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint",
+    'prettier',
     'eslint:recommended', // is standard for ESLint
-    'plugin:react/recommended',
-    "plugin:@typescript-eslint/eslint-recommended", // updates the eslint:recommended config to better suit Typescript
+    'plugin:prettier/recommended',
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/eslint-recommended', // updates the eslint:recommended config to better suit Typescript
     'plugin:@typescript-eslint/recommended', // is a set of opinionated rules that adhere to TypeScript best practices
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
-  plugins: ['react', "react-hooks", "header", '@typescript-eslint'],
+  plugins: ['react', 'react-hooks', 'header', '@typescript-eslint', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['./src'],
+      },
+    },
+  },
   rules: {
-    indent: ['error', 'tab'],
     'linebreak-style': ['error', 'unix'],
-  }
+  },
 };
